ec7a6cc9366700f7ae1cb0b4c4302c4103a7cbcb
Oluwadara Olabampe <daraolabampe1@gmail.com>
2024-04-23 17:23:31 +0100

    firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/compat/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKpxJsKDMFJ8oAvCN9kHbdGt3xmBEtviM",
+  authDomain: "mrmoney-6d0db.firebaseapp.com",
+  projectId: "mrmoney-6d0db",
+  storageBucket: "mrmoney-6d0db.appspot.com",
+  messagingSenderId: "679171873343",
+  appId: "1:679171873343:web:362abdcc09a7d33433f68a",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+export { projectFirestore, projectAuth };
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,6 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Signup.module.css";
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, email, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.signupform}>
+      <h2>Signup</h2>
+      <label>
+        <span>username:</span>
+        <input
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+      </label>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Signup</button>
+    </form>
+  );
 }
