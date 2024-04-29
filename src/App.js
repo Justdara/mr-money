14ab62909bcd17aa2606aa562df90d3b273a1d82
Oluwadara Olabampe <daraolabampe1@gmail.com>
2024-04-29 23:58:00 +0100

    redirect logic

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const { authIsReady, user } = useAuthContext();
+
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
-        </Routes>
-      </Router>
+      {authIsReady && (
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="login" />}
+            ></Route>
+            <Route
+              path="/login"
+              element={user ? <Navigate to="/" /> : <Login />}
+            ></Route>
+            <Route
+              path="/signup"
+              element={user ? <Navigate to="/" /> : <Signup />}
+            ></Route>
+          </Routes>
+        </Router>
+      )}
     </div>
   );
 }
