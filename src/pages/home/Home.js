dbaf174532baee5da81117c871822c7c910dc99d
Oluwadara Olabampe <daraolabampe1@gmail.com>
2024-04-30 17:23:38 +0100

    adding user transactions

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,17 @@
 import React from "react";
 import styles from "./Home.module.css";
+import TransactionForm from "./TransactionForm";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 export default function Home() {
-  return <div>Home</div>;
+  const { user } = useAuthContext();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>transaction list</div>
+      <div className={styles.side}>
+        <TransactionForm uid={user.uid} />
+      </div>
+    </div>
+  );
 }
